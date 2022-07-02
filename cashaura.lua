ade17efcd7263cf5677057b2cd75cdde697d58a4
Husam_Dev <72893731+Husam-Dev@users.noreply.github.com>
2022-07-02 03:50:19 +0200

    Add files via upload

--- cashaura.lua
+++ cashaura.lua
@@ -38,12 +38,11 @@ getgenv().CashAuraCMD = function(args)
                     if getgenv().CA_AutoCashPickup == false then
                         break
                     end
-                    for i, thechild in pairs(game.Workspace:WaitForChild('Ignored'):WaitForChild('Drop'):GetChildren()) do
-                        if thechild.Name == 'MoneyDrop' or thechild.Name == 'MoneyDropCounted' then
-                            if game.Players.LocalPlayer:DistanceFromCharacter(thechild.Position) < 13 then
-                                fireclickdetector(thechild:FindFirstChild('ClickDetector'), 12)
-                            end
+                    for _,v in pairs(game:GetService("Workspace").Ignored.Drop:GetChildren()) do
+                        if v.Name == "MoneyDrop" and math.floor((v.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).magnitude) <= 12 then
+                            fireclickdetector(v.ClickDetector)
                         end
+                        wait()
                     end
                     wait(1)
                 end
