700743f373b90e391ad5899d18637bfdbf657a94
Husam_Dev <72893731+Husam-Dev@users.noreply.github.com>
2022-08-24 16:27:06 +0200

    Create chestpump.lua

---
+++ chestpump.lua
@@ -0,0 +1,1 @@
+getgenv().CA_IsVibing=true;game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored=false;local a=game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")local b=Instance.new("Animation")b.AnimationId="http://www.roblox.com/asset/?id=3189779152"local c=a:LoadAnimation(b)c:Play()if ChatMessages then MUF_sendChatMessage('Vibing da hood ChestPump!')end
