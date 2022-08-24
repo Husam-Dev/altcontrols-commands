59e462579d2f1754f6e9f8f84d008968c7bd8b13
Husam_Dev <72893731+Husam-Dev@users.noreply.github.com>
2022-08-24 16:28:21 +0200

    Create monkey.lua

---
+++ monkey.lua
@@ -0,0 +1,1 @@
+getgenv().CA_IsVibing=true;game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored=false;local a=game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")local b=Instance.new("Animation")b.AnimationId="http://www.roblox.com/asset/?id=3333499508"local c=a:LoadAnimation(b)c:Play()if ChatMessages then MUF_sendChatMessage('Vibing Monkey Dance!')end
