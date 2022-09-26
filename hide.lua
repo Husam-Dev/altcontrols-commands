a3cad21c8b9ab777604742774b025af799dad5a3
Husam_Dev <72893731+Husam-Dev@users.noreply.github.com>
2022-09-26 19:02:13 +0200

    Create hide.lua

---
+++ hide.lua
@@ -0,0 +1,1 @@
+getgenv().ExecutedhideCMD=true;getgenv().hideCMD=function()game.Players.LocalPlayer.Character.HumanoidRootPart.Position=CFrame.new(0.8007601499557495,-58.702049255371094,146.635498046875)end
