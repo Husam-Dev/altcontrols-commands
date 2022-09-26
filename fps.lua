0433f947d1d590205cfcbbd489728e86e1ed6aeb
Husam_Dev <72893731+Husam-Dev@users.noreply.github.com>
2022-09-26 22:37:49 +0200

    Update fps.lua

--- fps.lua
+++ fps.lua
@@ -1,1 +1,1 @@
-getgenv().ExecutedfpsCMD=true;getgenv().fpsCMD=function()if args[2]then if tonumber(args[2])then setfpscap(tonumber(args[2]))end end end
+getgenv().ExecutedfpsCMD=true;getgenv().fpsCMD=function()if args[2]then if tonumber(args[2])then setfpscap(tonumber(args[2]))MUF_sendChatMessage("fps capped at "..args[2].." frame per second")end end end
