2e1b79c1d7e7145b9d0afb705b397bf0e676677e
Husam_Dev <72893731+Husam-Dev@users.noreply.github.com>
2022-09-26 19:01:00 +0200

    Update dance.lua

--- dance.lua
+++ dance.lua
@@ -1,17 +1,1 @@
-getgenv().ExecuteddanceCMD = true
-
-getgenv().danceCMD = function()
-    if CA_IsVibing then
-        for i,v in pairs(game.Players.LocalPlayer.Character.Humanoid:GetPlayingAnimationTracks()) do
-            v:Stop()
-        end
-        getgenv().CA_IsVibing = false
-    end
-    getgenv().CA_IsVibing = true
-    game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
-    local humanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
-    local animation = Instance.new("Animation")
-    animation.AnimationId = "http://www.roblox.com/asset/?id=3189776546"
-    local animationTrack = humanoid:LoadAnimation(animation)
-    animationTrack:Play()
-end
+getgenv().ExecuteddanceCMD=true;getgenv().danceCMD=function()if CA_IsVibing then for a,b in pairs(game.Players.LocalPlayer.Character.Humanoid:GetPlayingAnimationTracks())do b:Stop()end;getgenv().CA_IsVibing=false end;getgenv().CA_IsVibing=true;game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored=false;local c=game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")local d=Instance.new("Animation")d.AnimationId="http://www.roblox.com/asset/?id=3189776546"local e=c:LoadAnimation(d)e:Play()end
